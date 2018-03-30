65acb433de96c36e26fecf1370cde97ae647701c
Gorbjorn <Gorbjorn@pmail.com>
2018-03-30 17:00:05 +0800

    Added static files compatibility

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require("express");
 var app = express();
 
-app.get('/', (request, response) => {
-	response.send("Hello!");
-});
+// app.get('/', (request, response) => {
+// 	response.send("Hello!");
+// });
 
-// app.use(express.static('/'));
+app.use(express.static("./"));
+
+// app.use(express.static("assets"));
 
 app.listen(3000, () => {
 	console.log("Listening on port 3000.");
